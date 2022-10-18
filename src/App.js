47f5a3937d6dd898e2ff64efdaae3b367cfbc3c7
Girish <111488101+phyelements-admin@users.noreply.github.com>
2022-10-18 16:44:17 +0530

    useRef Demo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect} from 'react';
+import { useState, useEffect , useRef} from 'react';
 
 function App() {
-  console.log("rendered");
-  const[text,setText] = useState('home');
-  // below useEffect executes only when first time component rendered
-  // useEffect(()=>{console.log("button is clicked")},[]);
-  //  this useEffect executes only when text get change  
-  useEffect(()=>{console.log("button is clicked")},[text]);
+  
+  const inputRef = useRef(null);
+  
+  useEffect(()=>{
+    inputRef.current.focus();
+    
+  },[]);
+  
 
   return (
     <div className="App">
-      <button onClick={()=>{setText('home')}}>Home</button>
-      <button onClick={()=>{setText('about')}}>About</button>
-      <h1>{text}</h1>
+      <h1>Welcome to react</h1>
+      <input ref={inputRef} type="text" />
+      <br/>
+      <input type="text" />
     </div>
   );
 }
